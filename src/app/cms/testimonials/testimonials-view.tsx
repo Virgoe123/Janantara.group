1a365e2fb44e135166b0735ec0c799379dcd3b12
Rd Januar <ridwnv@gmail.com>
2025-09-17 05:57:17 +0000

    masa halaman halaman lain bisa membentang full sedangkan halaman testimo

--- src/app/cms/testimonials/testimonials-view.tsx
+++ src/app/cms/testimonials/testimonials-view.tsx
@@ -291,7 +291,7 @@ export default function TestimonialsView({ initialTestimonials }: { initialTesti
   }
   
   return (
-    <div className="space-y-6 flex flex-col">
+    <div className="space-y-6">
         <div className="flex items-center justify-between">
             <div className="space-y-1">
                 <h1 className="text-3xl font-bold font-headline tracking-tight">Testimonials</h1>
