d4df0d52c70026c801904d1ffc06410ce0ec84b1
Rd Januar <ridwnv@gmail.com>
2025-09-17 07:08:14 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,8 @@
 
 "use client";
 
-import { useActionState, useFormStatus } from "react";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { authenticate, LoginState } from "@/lib/actions";
 import { Button } from "@/components/ui/button";
 import {
